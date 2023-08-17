2d73bf8b44c1057b4f57727ededff427d6ba7488
SikroxMemer <simoarmymen9@gmail.com>
2023-08-17 12:42:43 +0100

    Added an alert info

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let Files = document.getElementById('Files');
 let Button = document.getElementById('Show');
 let MyForm = document.getElementById('myForm');
 let Reset = document.getElementById('Reset');
-//
+// // // // // // // // // // // // // // // //
 
 // Imports
 import './styles/main.scss';
@@ -11,7 +11,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import 'bootstrap/dist/js/bootstrap.js'
 import icon from './assets/icon.png';
-// 
+// // // // // // // // // // // // // // //
+
 
 document.getElementById('link').setAttribute(
     'href', icon
@@ -26,6 +27,8 @@ Reset.addEventListener('click', () => {
 });
 
 const BlobImages = new Array();
+
+
 
 Files.addEventListener('change', () => {
     for (let i = 0; i < Files.files.length; i++) {
@@ -43,9 +46,13 @@ Files.addEventListener('change', () => {
 Button.addEventListener(
     'click', () => {
 
-        if (BlobImages.length == 0) 
-        {
-            Button.disabled = false;
+        if (BlobImages.length == 0) {
+            document.getElementById("section").innerHTML = `
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Info :</strong> Try adding some images before clicking on show
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            `
         }
         else {
             Button.disabled = true;
@@ -53,15 +60,15 @@ Button.addEventListener(
 
         BlobImages.forEach((image) => {
 
-            let Div     = document.createElement('div')
-            let Image   = document.createElement('img');
+            let Div = document.createElement('div')
+            let Image = document.createElement('img');
             let Image_Section = document.createElement('div');
 
-            Image_Section.setAttribute('class' , 'd-flex flex-column')    
+            Image_Section.setAttribute('class', 'd-flex flex-column')
             Image.src = image;
 
-            
-        
+
+
             Image_Section.append(Image)
             Div.append(Image_Section)
 
@@ -70,4 +77,4 @@ Button.addEventListener(
 
             document.getElementById('Gallery').append(Div);
         })
-})
+    })
